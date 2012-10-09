5991a60eda2a494427a9f1c1dd897eb891e10422
Akos Toth <akos.toth@pludoni.de>
2012-10-09 16:37:47 +0200

    font-path customizable

--- lib/attribute_imagifiable.rb
+++ lib/attribute_imagifiable.rb
@@ -8,10 +8,14 @@ module AttributeImagifiable
 
 
   module ClassMethods
+    # options:
+    #   :as => :paperclip_attribute
+    #   :font => which system font to use
     def attribute_imagifiable(attribute, options={})
       raise ArgumentError.new("No :as given") if options[:as].nil?
-      @_imagifiable_attributes ||= {}
-      @_imagifiable_attributes[attribute] = options[:as]
+      @_imagifiable_attributes ||= []
+      font = options[:font] || "/usr/share/fonts/truetype/msttcorefonts/Verdana.ttf"
+      @_imagifiable_attributes << [attribute,  options[:as], font]
 
       if (not column_names.include? "#{options[:as]}_file_name") or !self.instance_methods.include?(options[:as])
         $stderr.puts "Add #{options[:as]} Paperclip attribute before using attribute_imagifiable"
@@ -22,13 +26,13 @@ module AttributeImagifiable
   private
   def generate_image
     attributes =  self.class.instance_variable_get("@_imagifiable_attributes")
-    attributes.each do |attribute, as|
+    attributes.each do |attribute, as, font|
       value = send(attribute)
       if send("#{attribute}_changed?") || !send(as).exists?
         if value.present?
           arg = Shellwords.escape(value)
           tmp_name = Rails.root.join("tmp/#{as}-#{SecureRandom.hex(10)}.jpg")
-          cmd = "convert -fill black -font /usr/share/fonts/truetype/msttcorefonts/Verdana.ttf -gravity center -size x40 label:#{arg} #{tmp_name}"
+          cmd = "convert -fill black -font #{font} -gravity center -size x40 label:#{arg} #{tmp_name}"
           unless system(cmd)
             raise RuntimeError.new "Image generation of #{attribute} failed. #{tmp_name}"
           end
